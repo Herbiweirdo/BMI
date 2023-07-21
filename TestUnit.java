5a437d0a38af1d39cdf193905e7c3d4467d3e472
herbert gese <herbertgese92@gmail.com>
2023-07-21 14:55:24 +1000

    First Commit for test cases.

---
+++ TestUnit.java
@@ -0,0 +1,35 @@
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestUnit 
+{
+    protected double w;
+    protected double h;
+    Base obj;
+
+    @Before
+    public void setUp()
+    {
+        w = 48.0;
+        h = 165.0;
+        obj = new Base.Child(w, h).build();
+    }
+
+    @After 
+    public void tearDown()
+    {
+        obj = null;
+    }
+
+    @Test 
+    /*
+     * Check to see if the build() method of class Child deos return an object
+     * of type Base.
+     */
+    public void test_the_build_method()
+    {
+        Base obj2 = new Base.Child(w, h).build();
+        assertEquals(obj, obj2 );
+    }
+}
